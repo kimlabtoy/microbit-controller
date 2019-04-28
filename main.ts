939124527aa7d344c661f63938631e39747712f0
kimlabtoy <48376994+kimlabtoy@users.noreply.github.com>
2019-04-28 08:07:27 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -182,9 +182,9 @@ namespace Kimlabmicrobit {
     /**
      * Do something when a button is pushed down or released. 
      */
-    //% blockId=BUTTOM block="Buttom |%myBtn|   %dir|" blockInlineInputs=true
+    //% blockId=BUTTON block="Button |%myBtn|   %dir|" blockInlineInputs=true
     //% weight=70
-    export function BUTTOM(myBtn: CBTM, dir: pushType, handler: Action): void {
+    export function BUTTON(myBtn: CBTM, dir: pushType, handler: Action): void {
         if (!init) {
             pin_init();
         }
